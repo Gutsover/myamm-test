d3a04c004bb87ec727c8314b2f7fbe494d061c9a
Gutsover <mylenevandaele@gmail.com>
2019-08-26 14:07:01 +0200

    add c modalCard for users

---
+++ src/app/user/modal-card/modal-card.component.ts
@@ -0,0 +1,16 @@
+import { Component, Input, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-modal-card',
+  templateUrl: './modal-card.component.html',
+  styleUrls: ['./modal-card.component.scss']
+})
+export class ModalCardComponent implements OnInit {
+
+  @Input() foodType = [];
+
+  constructor() { }
+
+  ngOnInit() {}
+
+}
