7174d6e1260b0d8a48624c3bf33acb8ad88bcee3
Gutsover <mylenevandaele@gmail.com>
2019-09-02 14:45:38 +0200

    add user-list in c create-user-card

--- src/app/admin/create-user-card/create-user-card.component.ts
+++ src/app/admin/create-user-card/create-user-card.component.ts
@@ -1,15 +1,31 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { Subscription } from 'rxjs';
+import { Utilisateur } from '../../models/Utilisateur.model';
+import { UtilisateurService } from '../../services/utilisateur.service';
 
 @Component({
   selector: 'app-create-user-card',
   templateUrl: './create-user-card.component.html',
   styleUrls: ['./create-user-card.component.scss']
 })
-export class CreateUserCardComponent implements OnInit {
+export class CreateUserCardComponent implements OnInit, OnDestroy {
 
-  constructor() { }
+  utilisateurs: Utilisateur[];
+  utilisateurSubscription: Subscription;
+
+  constructor(private utilisateurService: UtilisateurService) { }
 
   ngOnInit() {
+    this.utilisateurSubscription = this.utilisateurService.utilisateurSubject.subscribe(
+      (utilisateurs: Utilisateur[]) => {
+        this.utilisateurs = utilisateurs;
+      }
+    );
+    this.utilisateurService.emitUtilisateurs();
+  }
+
+  ngOnDestroy() {
+    this.utilisateurSubscription.unsubscribe();
   }
 
 }
