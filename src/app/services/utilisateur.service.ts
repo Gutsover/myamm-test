2bbf9233644842e98eb907570acb19d9cbfdc6ea
Gutsover <mylenevandaele@gmail.com>
2019-09-03 14:28:38 +0200

    modif option select

--- src/app/services/utilisateur.service.ts
+++ src/app/services/utilisateur.service.ts
@@ -10,7 +10,7 @@ export class UtilisateurService {
         },
         {
             name: 'Jean',
-            role: 'user',
+            role: 'agent',
             password: '5678'
         }
     ];
